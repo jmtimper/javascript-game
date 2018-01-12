27a6addc0969b35a9cb6b9b1b0c1f73cb42efff8
Jeremy Timperio <34565851+jmtimper@users.noreply.github.com>
2018-01-12 14:43:57 -0500

    #1 Created canvas for game to take place on

--- Pong/scripts/main.js
+++ Pong/scripts/main.js
@@ -1,3 +1,36 @@
-function start(){
-    document.getElementById("title").innerHTML = "Javascript function";
+//request animation frame that runs the game at 60fps
+var animate = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function(callback) { window.setTimeout(callback, 1000/60) };
+
+//render the 2d canvas
+var canvas = document.createElement('canvas');
+var width = 600;
+var height = 400;
+canvas.width = width;
+canvas.height = height;
+var context = canvas.getContext('2d');
+
+//starts the program
+window.onload = function() {
+    document.body.appendChild(canvas);
+    animate(step);
+}
+
+//updates all assets in game
+var step = function(){
+    update();
+    render();
+    animate(step);
+}
+
+//Todo
+var update = function() {
+};
+
+//render canvas based on vars
+var render = function() {
+    context.fillStyle = "#FF0000";
+    context.fillRect(0, 0, width, height);
 }
